9ca282919e94a7138a9d9cf30170124c40597f34
Salva <supersalva75@gmail.com>
2017-11-17 18:38:15 +0100

    Subimos todos los archivos

---
+++ src/Principal.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num;
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Introduzca un numero");
+		num=sc.nextInt();
+		impar(num);
+	}
+	public static void impar(int n)
+	{
+		int i;
+		int suma=0;
+		for(i=1;i<=n;i++) 
+		{
+			      
+		}
+		
+		
+
+	}
+
+}
+
